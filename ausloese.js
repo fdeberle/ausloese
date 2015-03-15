b9a94525f86a255a398f08595527f1b7aef820a9
Frank Deberle <f.deberle@gmail.com>
2015-03-15 20:42:26 +0100

    Year is now a function parameter. Use momentjs.

--- ausloese.js
+++ ausloese.js
@@ -1,27 +1,32 @@
-exports.calculateAusloeseFor2013 = function(numberOfHours) {
-    switch (true) {
-        case (numberOfHours < 8):
-            return 0;
-            break;
+var moment = require('moment');
 
-        case (numberOfHours < 14):
-            return 6;
-            break;
+exports.calculate = function(date, numberOfHours) {
+    date = moment(date);
+    if (date.year() === 2013) {
+        switch (true) {
+            case (numberOfHours < 8):
+                return 0;
+                break;
 
-        case (numberOfHours < 24):
-            return 12;
-            break;
+            case (numberOfHours < 14):
+                return 6;
+                break;
 
-        case (numberOfHours === 24):
-            return 24;
-            break;
+            case (numberOfHours < 24):
+                return 12;
+                break;
 
-        case (numberOfHours > 24):
-            throw 'Number of hours must not be more than 24 per day';
-            break;
+            case (numberOfHours === 24):
+                return 24;
+                break;
 
-        default:
-            return 0;
-            break;
+            case (numberOfHours > 24):
+                throw 'Number of hours must not be more than 24 per day';
+                break;
+
+            default:
+                return 0;
+                break;
+        }
     }
 };
--- spec/ausloese-spec.js
+++ spec/ausloese-spec.js
@@ -1,30 +1,30 @@
 var ausloese = require('../ausloese');
 
-describe('2013 und davor', function() {
-    describe('calculateAusloeseFor2013', function() {
+describe('2013 and earlier', function() {
+    describe('calculate Ausloese for 2013', function() {
         var amount;
-        it('should give back 6 Euro when absence is 8 or less than 14 hours', function() {
-            amount = ausloese.calculateAusloeseFor2013(8);
+        it('should give back 6 Euro when absence is 8 or less than 14 hours and date is 2013 and before', function() {
+            amount = ausloese.calculate('2013-12-31', 8);
             expect(amount).toBe(6);
-            amount = ausloese.calculateAusloeseFor2013(13.99);
+            amount = ausloese.calculate('2013-12-31', 13.99);
             expect(amount).toBe(6);
         });
         it('should give back 0 Euro when absence is lower than 8 hours', function() {
-            amount = ausloese.calculateAusloeseFor2013(7);
+            amount = ausloese.calculate('2013-12-31', 7);
             expect(amount).toBe(0);
         });
         it('should give back 12 Euro when absence is 14 or less than 24 hours', function() {
-            amount = ausloese.calculateAusloeseFor2013(14);
+            amount = ausloese.calculate('2013-12-31', 14);
             expect(amount).toBe(12);
-            amount = ausloese.calculateAusloeseFor2013(23.99);
+            amount = ausloese.calculate('2013-12-31', 23.99);
             expect(amount).toBe(12);
         });
         it('should give back 24 Euro when absence is 24 hours', function() {
-            amount = ausloese.calculateAusloeseFor2013(24);
+            amount = ausloese.calculate('2013-12-31', 24);
             expect(amount).toBe(24);
         });
         it('should throw an error when absence is more than 24 hours', function() {
-            expect( function() { ausloese.calculateAusloeseFor2013(25) }).toThrow(new Error('Number of hours must not be more than 24 per day'));
+            expect( function() { ausloese.calculate('2013-12-31', 25) }).toThrow(new Error('Number of hours must not be more than 24 per day'));
         });
     });
 });
