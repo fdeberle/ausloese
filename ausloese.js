ae1a6ebaf838d0efa7c50eb4536163edce853a30
Frank Deberle <f.deberle@gmail.com>
2015-03-16 08:16:05 +0100

    Spec for 2014. One missing.

--- ausloese.js
+++ ausloese.js
@@ -32,6 +32,29 @@ exports.calculate = function(date, numberOfHours) {
                     break;
             }
         }
+        if (year >= 2014) {
+            switch (true) {
+                case (numberOfHours < 8):
+                    return 0;
+                    break;
+
+                case (numberOfHours < 24):
+                    return 12;
+                    break;
+
+                case (numberOfHours === 24):
+                    return 24;
+                    break;
+
+                case (numberOfHours > 24):
+                    throw 'Number of hours must not be more than 24 per day';
+                    break;
+
+                default:
+                    return 0;
+                    break;
+            }
+        }
     } else {
         throw 'Date must be valid';
     }
--- spec/ausloese-spec.js
+++ spec/ausloese-spec.js
@@ -32,6 +32,38 @@ describe('When date is 2013 and earlier', function() {
     });
 });
 
+describe('When date is 2014 and later', function() {
+    describe('calculate', function() {
+        var amount;
+
+        // TODO
+        /*
+         jeweils 12 € für den An- und Abreisetag, wenn der Arbeitnehmer an diesem,
+         einem anschließenden oder vorhergehenden Tag außerhalb seiner Wohnung übernachtet,
+         eine Mindestabwesenheitszeit ist nicht erforderlich
+         */
+        it('should give back 0 Euro when absence is lower than 8 hours', function() {
+            amount = ausloese.calculate('2014-01-01', 7);
+            expect(amount).toBe(0);
+        });
+        it('should give back 12 Euro when absence is 8 or less than 24 hours', function() {
+            amount = ausloese.calculate('2014-01-01', 8);
+            expect(amount).toBe(12);
+            amount = ausloese.calculate('2014-01-01', 23.99);
+            expect(amount).toBe(12);
+        });
+        it('should give back 24 Euro when absence is 24 hours', function() {
+            amount = ausloese.calculate('2014-01-01', 24);
+            expect(amount).toBe(24);
+        });
+        it('should throw an error when absence is more than 24 hours', function() {
+            expect(function() {
+                ausloese.calculate('2014-01-01', 25)
+            }).toThrow(new Error('Number of hours must not be more than 24 per day'));
+        });
+    });
+});
+
 describe('When date is invalid', function() {
     describe('calculate', function() {
         it('should throw an error', function() {
